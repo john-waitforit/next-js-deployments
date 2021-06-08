a5d4e9ef06ff9b8582dd00bfdefbd58706446746
Jonathan Wagner <jonathanw@theodo.co.uk>
2021-06-08 18:55:42 +0200

    First commit

---
+++ pages/api/version.ts
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { VersionResponseData } from '../types'
+
+
+export default (req: NextApiRequest, res: NextApiResponse<VersionResponseData>) => {
+  res.status(200).json({ version: 'amplify-v1.0' })
+}
